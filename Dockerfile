45629549109d0bed633edc0939db17a0f7850235
MaxwellVizion <logan.kimmel@traxtech.com>
2016-02-12 09:59:25 -0600

    Working 3 tests

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM golang
+
+ADD . /go/src/tunneltest
+
+RUN cd /go/src/tunneltest && go get ./...
+RUN go install tunneltest
+WORKDIR /go/src/tunneltest
+ENTRYPOINT /go/bin/tunneltest
+
+EXPOSE 3001
---
+++ tunneltest.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+
+	r := httprouter.New()
+	r.GET("/", checkTunnels)
+
+	http.ListenAndServe(":3001", r)
+}
+
+func checkTunnels(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	endpoints := map[string]string{
+		"asmdev":   "20695",
+		"delljump": "20066",
+		"delllab":  "20125",
+	}
+	statuses := map[string]string{}
+	for name, port := range endpoints {
+		var status string
+		_, err := net.Dial("tcp", fmt.Sprintf("asm.lkimmel.com:%s", port))
+		if err != nil {
+			status = "offline"
+		} else {
+			status = "online"
+		}
+		statuses[name] = status
+	}
+	j, _ := json.Marshal(statuses)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "%s", j)
+}
